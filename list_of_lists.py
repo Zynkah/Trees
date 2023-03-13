0079e9b15456ad970f78d0950141cc02d8d09b5d
Zynkah <ffgurl07@yahoo.com>
2023-03-13 10:18:48 -0700

    List of Lists

---
+++ list_of_lists.py
@@ -0,0 +1,5 @@
+my_tree = ['a', ['b', ['d', [], []], ['e', [], [] ] ], ['c', ['f', [], []] ]]
+print(my_tree)
+print('left subtree = ', my_tree[1]) # ['b', ['d', [], []], ['e', [], []]]
+print('root = ', my_tree[0]) # a
+print('right subtree = ', my_tree[2]) # ['c', ['f', [], []]]
